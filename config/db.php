76feca7a295dc841196486ddff39591f16967da5
longjianghu <215241062@qq.com>
2019-10-11 08:35:26 +0800

    调整数据库配置

--- config/db.php
+++ config/db.php
@@ -2,7 +2,6 @@
 
 return [
     'config'  => [
-        'charset'   => env('DB_CHARSET'),
         'collation' => env('DB_COLLATION'),
         'strict'    => false,
         'timezone'  => '+8:00',
